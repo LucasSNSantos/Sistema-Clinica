f0bba3d6691421899716563b11d4d1912fb404ce
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-10-03 16:56:33 -0300

    Pacientes e Medicos Criados

---
+++ Elementos.h
@@ -0,0 +1,59 @@
+#ifndef ELEMENTOS_H
+#define ELEMENTOS_H
+#include<string.h>
+
+typedef struct reg_medico{
+	int idade;
+	char nome[50];
+	//lista de pacientes pra serem atendidos
+}Medico;
+
+Medico registra_medico(int i, char* nome){
+	Medico c;
+	c.idade = i;
+	strcpy(c.nome,"Dr.");
+	strcat(c.nome, nome);
+	return c;
+}
+
+void printa_medico(Medico doc){
+	printf("Idade: %d \n", doc.idade);
+	printf("Eu sou o %s", doc.nome);
+}
+
+
+typedef struct reg_paciente{
+	int prioridade;
+	char nome[50];
+	char cpf[20];
+	int idade;
+} Paciente;
+
+Paciente registra_paciente(int i, char* n, char* cpf, int pri){
+	Paciente novo;
+	strcpy(novo.nome, n);
+	strcpy(novo.cpf,cpf);
+	novo.idade = i;
+	if(pri > 3){
+		novo.prioridade = 3;
+	}else if(pri < 1){
+		novo.prioridade = 1;
+	}else{
+		novo.prioridade = pri;
+	}
+	return novo;
+}
+
+void printa_paciente(Paciente p){
+	printf("Ficha do Paciente \n");
+	printf("Nome: %s\n", p.nome);
+	printf("Idade: %d", p.idade);
+	printf("CPF: %s \n", p.cpf);
+	printf("Status de Prioridade: %d", p.prioridade);
+}
+
+
+
+
+
+#endif
--- Main.c
+++ Main.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-
+#include"Elementos.h"
 
 int main(void){
 	
 	char a[] = "Sistema da Clinica";
 	
-	printf("%s", a);
+	//printf("%s", a);
 	
-	
-	
-	
+	//Paciente p = registra_paciente(19,"Joao","32343234",2);
+	//printa_paciente(p);
 	
 	return 0;
 }
