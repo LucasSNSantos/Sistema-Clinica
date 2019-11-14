145ed0333419894bbfb32e11ef7e3c9731a55382
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-11-14 07:54:34 -0300

    we will re-do this Project
    Fixed this

--- Main.c
+++ Main.c
@@ -1,35 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include"Elementos.h"
-#include"Interface.h"
+#include<stdio.h>
 
 
 
-main(){
-	int en,  x = 0;
-	while(x==0){
+int main()
+{
 	
-		puts("Bem Vindo a Clinica BCSB:");
-		puts("___________________________________________________________________________________________________________\n");
-		puts("Pressione 1 para registrar");
-		puts("Pressione 2 para visualizar");
-		puts("Pressione 3 para sair");
-		scanf("%d", &en);
-		system("CLS");
-		switch(en){
-			case 1:
-				menu1();
-				break;
-			case 2:
-				menu2();
-				break;
-			case 3:
-				x = 1;
-				break;
-			default:
-				puts("ERRO: comando invalido\n\n");
-				break;
-		}
-	}
+	printf("Hello World");
+	
+	
+	
 }
-
