ac7a9240c383f9d90f81056705c55636b09db53d
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-11-14 10:17:06 -0300

    Tested and created Fila de Pacientes

--- FilaPacientes.h
+++ FilaPacientes.h
@@ -16,6 +16,7 @@ struct fila{
 typedef struct fila Fila;
 
 
+//Iniciando a fila na memória
 Fila* cria_fila()
 {
 	Fila* f;
@@ -30,10 +31,43 @@ Fila* cria_fila()
 	return NULL;
 }
 
+int fila_cheia(Fila* f)
+{
+	return f->cont == MAX;
+}
+
+int fila_vazia(Fila* f)
+{
+	return f->cont == 0;
+}
+
+//Inserir paciente na fila 
+int inserir_paciente(Fila* f, Paciente p)
+{
+	if(f == NULL || fila_cheia(f)) return 0;
+	f->pacientes[f->fim] = p;
+	f->fim = (f->fim+1)%MAX;
+	f->cont++;
+	return 1;
+}
 
 
 
-
+//removendo pacien
+Paciente sair_da_fila(Fila* f)
+{
+	Paciente temp;
+	if(f != NULL || fila_vazia(f))
+	{
+		temp = f->pacientes[f->inicio];
+		f->inicio = (f->inicio+1) % MAX;
+		f->cont--;
+		return temp;
+	}else
+	{
+		return temp;
+	}
+}
 
 
 
--- objetos.h
+++ objetos.h
@@ -10,6 +10,7 @@
 
 //Estrutura do Paciente
 struct paciente{
+	int codigo;
 	int idade;
 	char Nome[30];
 	Prioridade prioridade;
@@ -20,15 +21,29 @@ struct paciente{
 typedef struct paciente Paciente;
 
 
-Paciente Registro(int idade, char* nome,int prioridade, char sexo, char* CPF, char* anam)
+//Registrando Paciente;
+Paciente Registro(int codigo,int idade, char* nome,int prioridade, char sexo, char* CPF, char* anam)
 {
 	Paciente f;
+	f.codigo = codigo;	
 	f.idade = idade;
 	strcpy(f.Nome,nome);
-	
+	f.prioridade = prioridade;
+	f.Sexo = sexo;
+	strcpy(f.CPF,CPF);
+	strcpy(f.Anamnese,anam);
+	return f;
 }
 
-
+//Printando o paciente
+void printa_paciente(Paciente p)
+{
+	printf("\n");
+	printf("Nome: %s\n", p.Nome);
+	printf("Idade: %d \n", p.idade);
+	printf("Sexo: %c \n",p.Sexo);
+	printf("Anamnese: %s\n", p.Anamnese);
+}
 
 
 
