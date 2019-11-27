558c2868071b0e39d91a36ffe047d0674cb45486
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-11-27 00:25:10 -0300

    Fila de prioridades Implementada
    Fila de prioridades implementada
    subdivisão entre as filas simples

--- FilaPrioridade.h
+++ FilaPrioridade.h
@@ -1,15 +1,61 @@
 #ifndef FILA_PRIORITARIA_H
 #define FILA_PRIORITARIA_H
 #include"FilaSimples.h"
-#define No_Fila Fila_Simples
+
 
 //Estrutura que vai agrupar as filas de acordo com a prioridade
 typedef struct no_fila_prioridade
-{  
-	No_Fila* prox;
-	No_Fila* ante;
-	
+{
+	  Fila_Simples* inicio;
+	  Fila_Simples* final;
 } Fila_Prioridade;
+
+Fila_Prioridade* inicia_fila_de_prioridades()
+{
+	Fila_Prioridade* novo = (Fila_Prioridade*) malloc(sizeof(Fila_Prioridade));
+	if(novo != NULL)
+	{
+		novo->inicio = NULL;
+		novo->inicio = NULL;
+		return novo;
+	}
+	return NULL;
+}
+
+int cria_subfila_com_prioridade(int prioridade_da_fila, Fila_Prioridade* fila)
+{
+	if(fila == NULL) return 0;
+	
+	if(fila->inicio == NULL)
+	{
+		fila->inicio = inicio_da_fila_simples(prioridade_da_fila);
+		fila->final = fila->inicio;
+		return 1;
+	}
+	
+	Fila_Simples* aux;
+	aux = fila->inicio;
+	while(aux->prox_fila != NULL)
+	{
+		aux = aux->prox_fila;
+	}
+	aux->prox_fila = inicio_da_fila_simples(prioridade_da_fila);	
+	fila->final = aux->prox_fila;	
+}
+
+int insere_paciente_com_prioridades(Fila_Prioridade* fila, Paciente p)
+{
+	Prioridade pri = p.prioridade;
+	if(fila == NULL) return 0;
+	Fila_Simples* aux;
+	aux = fila->inicio;
+	while(aux->prox_fila != NULL || aux->prioridade_da_fila != pri)
+	{
+		aux = aux->prox_fila;
+	}
+	adiciona_na_fila_simples(aux,p);
+	return 1;
+}
 
 
 
--- FilaSimples.h
+++ FilaSimples.h
@@ -10,12 +10,12 @@ typedef struct no_simples
 {
 	Paciente no_paciente;
 	struct no_simples * prox;
-	struct no_simples * ante;			
 } No_Paciente;
 
 //Estrutura da fila simples, que serão agrupadas por prioridade
 typedef struct no_fila
 {
+	int prioridade_da_fila;	
 	struct no_fila* prox_fila;
 	No_Paciente* inicio;
 	No_Paciente* final;
@@ -28,7 +28,6 @@ No_Paciente* registra_no_Paciente(Paciente p)
 	if(novo != NULL)
 	{
 		novo->no_paciente = p;
-		novo->ante = NULL;
 		novo->prox = NULL; 
 		return novo;
 	}
@@ -36,11 +35,12 @@ No_Paciente* registra_no_Paciente(Paciente p)
 }
 
 
-Fila_Simples* inicio_da_fila_simples()
+Fila_Simples* inicio_da_fila_simples(int pri)
 {
 	Fila_Simples* novo = (Fila_Simples*) malloc(sizeof(Fila_Simples));
 	if(novo != NULL)
 	{
+		novo->prioridade_da_fila = pri;
 		novo->inicio = NULL;
 		novo->final = NULL;
 		novo->prox_fila = NULL;
@@ -49,18 +49,53 @@ Fila_Simples* inicio_da_fila_simples()
 	return NULL;
 }
 
-int adiciona_na_fila_simples(Fila_Simples* fi,Paciente p)
+int adiciona_na_fila_simples(Fila_Simples* fi, Paciente p)
 {
 	if(fi == NULL) return 0;
 	if(fi->inicio == NULL)
 	{
 		fi->inicio = registra_no_Paciente(p);
 		fi->final = fi->inicio;
-		
+		return 1;
 	}
 	
+	No_Paciente* aux;
+	aux = fi->inicio;
+	while(aux->prox != NULL)
+	{
+		aux = aux->prox;
+	}
 	
-	
+	aux->prox = registra_no_Paciente(p);
+	fi->final = aux->prox;	
+	return 1;
+}
+
+Paciente desenfileira_fila_simples(Fila_Simples* fi)
+{
+	Paciente temp;
+	if(fi == NULL || fi->inicio == NULL)
+	{
+		return temp;	
+	} 
+	temp = fi->inicio->no_paciente;
+	fi->inicio = fi->inicio->prox;
+	return temp;
+}
+
+int tamanho_da_fila_simples(Fila_Simples* fi)
+{
+	int tam = 0;
+	if(fi == NULL || fi->inicio == NULL) return 0;
+	else
+	{
+		No_Paciente* aux = fi->inicio;
+		while(aux->prox != NULL)
+		{
+			tam++;
+		}
+		return tam;
+	}
 }
 
 
